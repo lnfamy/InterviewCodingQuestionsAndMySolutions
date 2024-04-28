65cda9636934319a966de8d1bb284e343b337da5
RIVER MALACHI <rivermalachi@gmail.com>
2024-04-28 16:22:01 +0300

    rahh

--- LeetCode/Discuss/000 Create Densest Rectangle.py
+++
@@ -1,9 +0,0 @@
-"""
-You are given the coordinates of a set of points in X and Y axis as an array of pair of integers.
-You need to find the densest rectangle that can be created from these points.
-
-The density of the rectangle is defined as the number of points inside the rectangle divided by the area
-of the rectangle. Once you have found out the densest rectangle, return the density of the rectangle.
-
-If it is not possible to create any rectangle from the given set of points, return -1.0
-"""
